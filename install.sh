b4834f2d3e1e426bb8cec41e7f145274e82b140a
Vinicius Souza <vinicius.souza@intelbras.com.br>
2020-11-24 21:32:41 -0300

    Add install graphviz to install.sh
    Signed-off-by: Vinicius Souza <vinicius.souza@intelbras.com.br>

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 SCRIPT=$(readlink -f "$0")
 PROGRAMDIR=$(dirname "$SCRIPT")
 
+sudo apt install graphviz -y
 pip3 install -q -r $PROGRAMDIR/requirements.txt
 
 sudo ln -sf $PROGRAMDIR/main.py /usr/sbin/omci_analyser
