58b9f1c687a48fbdf6bb6317e67c75216fdcc82d
Vinicius Souza <vinicius.souza@intelbras.com.br>
2020-11-25 09:07:47 -0300

    Add install for python3-pip
    Signed-off-by: Vinicius Souza <vinicius.souza@intelbras.com.br>

--- install.sh
+++ install.sh
@@ -3,14 +3,17 @@
 SCRIPT=$(readlink -f "$0")
 PROGRAMDIR=$(dirname "$SCRIPT")
 
-echo "# Installing Graphviz ..."
-sudo apt install graphviz -y &>/dev/null
+echo -e "\n# Installing Graphviz ...\n"
+sudo apt install graphviz -y
 
-echo "# Installing python dependencies ..."
-pip3 install -q -r $PROGRAMDIR/requirements.txt
+echo -e "\n# Installing PIP3 ...\n"
+sudo apt install python3-pip -y
 
-echo "# Creating symbolic link ..."
+echo -e "\n# Installing python dependencies ...\n"
+pip3 install -r $PROGRAMDIR/requirements.txt
+
+echo -e "\n# Creating symbolic link ...\n"
 sudo ln -sf $PROGRAMDIR/main.py /usr/sbin/omci_analyser
 sudo chmod 777 /usr/sbin/omci_analyser
 
-echo "Installation completed!"
+echo -e "Installation completed!"
